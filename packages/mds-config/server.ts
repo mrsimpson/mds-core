f1b43cfe394c2822dab9f7560a8c4274424fea42
Mark Maxham <mark.maxham@gmail.com>
2020-03-09 11:30:22 -0700

    update dates in license

--- packages/mds-config/server.ts
+++ packages/mds-config/server.ts
@@ -1,5 +1,5 @@
 /*
-    Copyright 2019 City of Los Angeles.
+    Copyright 2019-2020 City of Los Angeles.
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
--- packages/mds-event-server/tests/index.spec.ts
+++ packages/mds-event-server/tests/index.spec.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
 /* eslint-disable promise/prefer-await-to-callbacks */
 /*
-    Copyright 2019 City of Los Angeles.
+    Copyright 2019-2020 City of Los Angeles.
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
