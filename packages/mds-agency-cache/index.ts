7bae060e64ebb9736a46e0985caac90dfd9c78b5
Neil Goldader <neilgoldader@gmail.com>
2020-07-30 16:49:30 -0400

    mds-agency-cache building

--- packages/mds-agency-cache/index.ts
+++ packages/mds-agency-cache/index.ts
@@ -18,16 +18,7 @@ import logger from '@mds-core/mds-logger'
 
 import flatten from 'flat'
 import { NotFoundError, nullKeys, stripNulls, now, isInsideBoundingBox, routeDistance } from '@mds-core/mds-utils'
-import {
-  UUID,
-  Timestamp,
-  Device,
-  VehicleEvent,
-  Telemetry,
-  BoundingBox,
-  EVENT_STATES_MAP,
-  VEHICLE_STATES
-} from '@mds-core/mds-types'
+import { UUID, Timestamp, Device, VehicleEvent, Telemetry, BoundingBox, VEHICLE_STATES } from '@mds-core/mds-types'
 import redis from 'redis'
 import bluebird from 'bluebird'
 
@@ -265,7 +256,7 @@ async function writeEvent(event: VehicleEvent) {
   // FIXME cope with out-of-order -- check timestamp
   // logger.info('redis write event', event.device_id)
   try {
-    if (event.event_type === 'deregister') {
+    if (event.event_types.includes('decommissioned') && event.vehicle_state === 'removed') {
       return await wipeDevice(event.device_id)
     }
     const prev_event = parseEvent((await hread('event', event.device_id)) as StringifiedEventWithTelemetry)
@@ -417,7 +408,7 @@ async function readDevicesStatus(query: {
       try {
         const parsedItem = parseEvent(item)
         if (
-          EVENT_STATES_MAP[parsedItem.event_type] === VEHICLE_STATES.removed ||
+          parsedItem.vehicle_state === VEHICLE_STATES.removed ||
           !parsedItem.telemetry ||
           (strictChecking && !isInsideBoundingBox(parsedItem.telemetry, query.bbox))
         )
