2a26131e48179aa0a73f8a15b50c30974b13cb65
Jane Huang <jane.x.huang@gmail.com>
2020-10-19 17:09:55 -0400

    minor fixes

--- packages/mds-compliance-engine/engine/helpers.ts
+++ packages/mds-compliance-engine/engine/helpers.ts
@@ -14,7 +14,6 @@ import db from '@mds-core/mds-db'
 import { isDefined, now, RuntimeError } from '@mds-core/mds-utils'
 import moment from 'moment-timezone'
 import { providers } from '@mds-core/mds-providers'
-import { AllowedProviderIDs } from './constants'
 import { MatchedVehicleInformation, VehicleEventWithTelemetry } from '../@types'
 
 const { env } = process
@@ -23,29 +22,6 @@ const TWO_DAYS_IN_MS = 172800000
 
 export function isPolicyUniversal(policy: Policy) {
   return !policy.provider_ids || policy.provider_ids.length === 0
-}
-
-export function clientCanViewPolicyCompliance(
-  provider_id: string,
-  queried_provider_id: string | undefined,
-  policy: Policy
-): boolean {
-  return (
-    policy &&
-    // True if the client is looking at a policy that applies to them
-    ((policy.provider_ids && policy.provider_ids.includes(provider_id)) ||
-      // True if a policy has no provider_ids, meaning it applies to every provider
-      isPolicyUniversal(policy) ||
-      /* True if the client is one of the allowed providers, and either the policy applies to the
-       * provider that was queried for, or the policy applies to every provider
-       */
-      (AllowedProviderIDs.includes(provider_id) &&
-        ((policy.provider_ids &&
-          policy.provider_ids.length !== 0 &&
-          queried_provider_id &&
-          policy.provider_ids.includes(queried_provider_id)) ||
-          isPolicyUniversal(policy))))
-  )
 }
 
 export async function getComplianceInputs(provider_id: string | undefined) {
