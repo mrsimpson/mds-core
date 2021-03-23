9b22ee994d0d794ca6fd6aeafe5e54d52890ae97
Neil Goldader <neilgoldader@gmail.com>
2021-03-23 10:55:14 -0400

    Clean up comments

--- packages/mds-agency/request-handlers.ts
+++ packages/mds-agency/request-handlers.ts
@@ -66,26 +66,15 @@ export const registerVehicle = async (req: AgencyApiRegisterVehicleRequest, res:
   const recorded = now()
 
   const { provider_id, version } = res.locals
+
   if (!version || version === '0.4.1') {
-    // convert old body into new body
+    // TODO: Transform 0.4.1 -> 1.0.0
   }
-  // const { device_id, vehicle_id, type, propulsion, year, mfgr, model } = body
+
   const { device_id, vehicle_id, vehicle_type, propulsion_types, year, mfgr, model } = body
 
   const status: VEHICLE_STATE = 'removed'
 
-  // const device = {
-  //   provider_id,
-  //   device_id,
-  //   vehicle_id,
-  //   type,
-  //   propulsion,
-  //   year,
-  //   mfgr,
-  //   model,
-  //   recorded,
-  //   status
-  // }
   const device = {
     provider_id,
     device_id,
