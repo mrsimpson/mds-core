599d891881d173efb57db8c0ab79a4ef0d96cbe8
Russen Guggemos <russen.guggemos@lacuna.ai>
2022-04-15 15:22:09 -0400

    PLAT-509 create telemetry annotation indicies, if not exists (#990)
    * telemetry annotation indices should be created if not exists, to allow for pre-deployment execution

---
+++ .changeset/tame-llamas-scream.md
@@ -0,0 +1,5 @@
+---
+"@mds-core/mds-ingest-service": patch
+---
+
+indicies should be created if not exists, to allow for pre-deployment execution
--- packages/mds-ingest-service/repository/migrations/1649954300329-AddTelemetryAnnotationIndicies.ts
+++ packages/mds-ingest-service/repository/migrations/1649954300329-AddTelemetryAnnotationIndicies.ts
@@ -5,22 +5,22 @@ export class AddTelemetryAnnotationIndicies1649954300329 implements MigrationInt
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE INDEX "idx_provider_id_h3_13_timestamp_telemetry_annotations" ON "telemetry_annotations" ("provider_id", "h3_13", "timestamp") `
+      `CREATE INDEX IF NOT EXISTS "idx_provider_id_h3_13_timestamp_telemetry_annotations" ON "telemetry_annotations" ("provider_id", "h3_13", "timestamp") `
     )
     await queryRunner.query(
-      `CREATE INDEX "idx_provider_id_h3_12_timestamp_telemetry_annotations" ON "telemetry_annotations" ("provider_id", "h3_12", "timestamp") `
+      `CREATE INDEX IF NOT EXISTS "idx_provider_id_h3_12_timestamp_telemetry_annotations" ON "telemetry_annotations" ("provider_id", "h3_12", "timestamp") `
     )
     await queryRunner.query(
-      `CREATE INDEX "idx_provider_id_h3_11_timestamp_telemetry_annotations" ON "telemetry_annotations" ("provider_id", "h3_11", "timestamp") `
+      `CREATE INDEX IF NOT EXISTS "idx_provider_id_h3_11_timestamp_telemetry_annotations" ON "telemetry_annotations" ("provider_id", "h3_11", "timestamp") `
     )
     await queryRunner.query(
-      `CREATE INDEX "idx_provider_id_h3_10_timestamp_telemetry_annotations" ON "telemetry_annotations" ("provider_id", "h3_10", "timestamp") `
+      `CREATE INDEX IF NOT EXISTS "idx_provider_id_h3_10_timestamp_telemetry_annotations" ON "telemetry_annotations" ("provider_id", "h3_10", "timestamp") `
     )
     await queryRunner.query(
-      `CREATE INDEX "idx_provider_id_h3_09_timestamp_telemetry_annotations" ON "telemetry_annotations" ("provider_id", "h3_09", "timestamp") `
+      `CREATE INDEX IF NOT EXISTS "idx_provider_id_h3_09_timestamp_telemetry_annotations" ON "telemetry_annotations" ("provider_id", "h3_09", "timestamp") `
     )
     await queryRunner.query(
-      `CREATE INDEX "idx_provider_id_h3_08_timestamp_telemetry_annotations" ON "telemetry_annotations" ("provider_id", "h3_08", "timestamp") `
+      `CREATE INDEX IF NOT EXISTS "idx_provider_id_h3_08_timestamp_telemetry_annotations" ON "telemetry_annotations" ("provider_id", "h3_08", "timestamp") `
     )
   }
 
